f65c4352587a379d483813004d382cffa86bee9b
omar-arif <omar.k.arif98@gmail.com>
2024-07-28 00:19:52 +0100

    operational first code

--- Code/llm_inference.py
+++ Code/llm_inference.py
@@ -2,18 +2,31 @@ import streamlit as st
 from pypdf import PdfReader
 import docx2txt
 
-st.write("Hello! Welcome to my new app")
-uploaded_file = st.file_uploader('upload your resume (pdf or docx file)', type=["pdf","docx"])
-if uploaded_file is not None:
-    suffix = uploaded_file.name.split(".")[-1]
-    cv_text = ""
+def ingest_document(stream):
+    '''
+        A function that takes a BytesIO in-memory stream, decodes the content into a string
+        then returns it.
+        the stream is either from a pdf or a docx file.
+    '''
+
+    suffix = stream.name.split(".")[-1]
+    content_text = ""
 
     match suffix:
         case "pdf":
-            reader = PdfReader(uploaded_file) 
+            reader = PdfReader(stream) 
             for page in reader.pages:
-                cv_text += page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False, layout_mode_scale_weight=1.0)
-            st.write(cv_text)
+                content_text += page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False, layout_mode_scale_weight=1.0)
         case "docx":
-            cv_text = docx2txt.process(uploaded_file)
-            st.write(cv_text)
+            content_text = docx2txt.process(stream)
+    
+    return content_text
+
+
+
+st.write("Hello! Welcome to my new app")
+uploaded_file = st.file_uploader('upload your resume (pdf or docx file)', type=["pdf","docx"])
+if uploaded_file is not None:
+    resume_text = ingest_document(uploaded_file)
+    st.write(resume_text)
+    
